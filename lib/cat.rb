76fc8baa2d81ba77d4405b319cc39180899490fa
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-30 18:35:04 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,8 +2,8 @@ class Cat
   # code goes here
   attr_reader :name, :mood
 
-  def initialize(name, mood)
+  def initialize(name)
     @name = name
-    @mood = mood
+    @mood = "nervous"
   end
 end
